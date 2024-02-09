c778e73fa3af59c2f9bb5435f1aeef54ecfbd171
arsalan haidar <2765162h@student.gla.ac.uk>
2024-02-09 16:12:41 +0000

    task 10.4 // implementing a test cookie

--- rango/views.py
+++ rango/views.py
@@ -21,10 +21,17 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
+    request.session.set_test_cookie()
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     context_dict = {'boldmessage': 'Silky, scrumptious, salty, serendipitous, salad!'}
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
